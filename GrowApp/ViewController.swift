3dcdde82df8184fbebf41758421bd35ad14c46e7
sdtran14 <sdtran@usc.edu>
2023-04-21 23:48:11 -0700

    Initial Commit

---
+++ GrowApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GrowApp
+//
+//  Created by Spencer Tran on 4/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
